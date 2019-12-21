a79e2cce94d25b74e456c0267322fa91aa3379db
Flavius Stroe <stroe.flavius@gmail.com>
2019-12-21 14:13:25 +0000

    Added new function for total masts count.

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,16 @@
 import csv
+import json
+from collections import Counter
 
 
-def read_csv():
+def read_csv() -> object:
 
     data = csv.DictReader(open('dataset/bink_dataset.csv'))
 
     return data
 
 
-def sorted_list(csv_dict: dict, parameter: str):
+def sorted_list(csv_dict: dict, parameter: str) -> list:
 
     new_array = []
 
@@ -20,7 +22,7 @@ def sorted_list(csv_dict: dict, parameter: str):
     return new_array
 
 
-def master_list_filtered(csv_dict: dict, parameter_to_filter_by: str, filter_by: str):
+def master_list_filtered(csv_dict: dict, parameter_to_filter_by: str, filter_by: str) -> list:
 
     master_array = []
 
@@ -31,11 +33,21 @@ def master_list_filtered(csv_dict: dict, parameter_to_filter_by: str, filter_by:
     return master_array
 
 
-def calculate_total_rent(array: list):
+def calculate_total_rent(list_of_properties: list) -> float:
 
     total_rent = 0
 
-    for property_info in array:
+    for property_info in list_of_properties:
         total_rent += float(property_info['Current Rent'])
 
     return total_rent
+
+
+def count_total_masts_by_tenant(csv_dict: dict) -> dict:
+
+    tenant_list = []
+
+    for tenant in csv_dict:
+        tenant_list.append(tenant['Tenant Name'])
+
+    return json.dumps(Counter(tenant_list), sort_keys=True, indent=2)
