1c458f9bdf657ce351ed08cfcc228a1553546acc
Flavius Stroe <stroe.flavius@gmail.com>
2019-12-21 15:11:01 +0000

    Added new func to extract tenant data betw. dates

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from datetime import datetime
 from collections import Counter
 
 
@@ -50,3 +51,20 @@ def count_total_masts_by_tenant(csv_dict: dict) -> dict:
     [tenant_list.append(tenant['Tenant Name']) for tenant in csv_dict]
 
     return json.dumps(Counter(tenant_list), sort_keys=True, indent=2)
+
+
+def tenant_data_between_dates(csv_dict: dict, start_date: str, end_date: str):
+
+    master_array = []
+
+    for property_info in csv_dict:
+        if datetime.strptime(property_info['Lease Start Date'], '%d-%b-%y') >= datetime.strptime(start_date, '%d-%b-%Y') and datetime.strptime(property_info['Lease Start Date'], '%d-%b-%y') <= datetime.strptime(end_date, '%d-%b-%Y'):
+            master_array.append(
+                property_info
+            )
+
+    for date in master_array:
+        date['Lease Start Date'] = datetime.strptime(date['Lease Start Date'], '%d-%b-%y').strftime('%d/%m/%Y')
+        date['Lease End Date'] = datetime.strptime(date['Lease End Date'], '%d-%b-%y').strftime('%d/%m/%Y')
+
+    return master_array
